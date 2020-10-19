97b5a382e593da01b37af4f8988cf14e06f15f80
Zoran Gajic <zorangajic@zorans-mbp.nygenome.org>
2020-10-19 15:00:02 -0400

    Test 1 containing code.

---
+++ ZG_blog.R
@@ -0,0 +1,9 @@
+## 2) In the file, please describe one of the ways that Wilson et. al. suggest is a 
+## good practice for computational reproducibility and how a tool like R can be used. 
+## Write your answer below.
+
+
+## 3) Add a code chunk and find the square root of 4 to the 13th power. If you are 
+## unsure of what command to use, google it. 
+
+sqrt(4)^13
