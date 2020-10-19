57d1e3344544bfeaf8652088bd3d1e61ed04e161
Zoran Gajic <zorangajic@zorans-mbp.nygenome.org>
2020-10-19 15:31:45 -0400

    Added in answers to Q2.

--- ZG_blog.R
+++ ZG_blog.R
@@ -2,6 +2,32 @@
 ## good practice for computational reproducibility and how a tool like R can be used. 
 ## Write your answer below.
 
+## I will comment on one of each section in the publication:
+
+## Data Management:
+## R allows for data manipulation without modificaiton of the original dataset
+## In addition, you can even save intermediate steps as .rds files without needing
+## to convert results to a standard data format (e.g. .csv)
+
+## Software
+## R has a very extensive and organized list of packages which all are version controlled
+## allowing you to not only specify which software was used specifically, but also 
+## allows other to run the exact same code instead of trying to re-code an analysis.  
+
+## Collaboration:
+## R Studio is already integrated with software such as Git that allows for collaborative work.
+## R is also supported by jupyter notebook, if that's your ideal collaborative enviornment.
+
+## Project Organization:
+## R projects and Git integration make this very easy, and there are tons of tutorials for 
+## directory structuring. 
+
+## Keeping track of Changes:
+## Git.
+
+## Manuscript:
+## R Studio allows export of .Rmd files which allows you to easily share your work.
+
 
 ## 3) Add a code chunk and find the square root of 4 to the 13th power. If you are 
 ## unsure of what command to use, google it. 
